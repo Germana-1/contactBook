88aec29422732c0317ec26ce739d085618342a0a
Germana-1 <germanagerriane@yahoo.com.br>
2023-04-09 21:43:11 -0300

    feat: deploy configs

--- back/build.sh
+++ back/build.sh
@@ -7,5 +7,6 @@ yarn
 yarn add typescript --dev
 npm i --save-dev @types/express
 npm i --save-dev @types/cors
+npm i --save-dev @types/bcryptjs
 yarn build
 yarn typeorm migration:run -d dist/data-source
